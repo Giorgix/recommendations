916925221b91655254167c19be4700fc058f0663
Giorgix <jackosinapsis@gmail.com>
2015-01-07 23:12:04 +0100

    added pearson correlation for two list of numbers

--- clusters.py
+++ clusters.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def readfile(filename):
     lines = [line for line in file(filename)]
 
@@ -12,3 +15,26 @@ def readfile(filename):
         # The data for this row is the remainder of the row
         data.append([float(x) for x in p[1:]])
     return rownames, colnames, data
+
+
+def pearson(v1, v2):
+    # Simple sums
+    sum1 = sum(v1)
+    sum2 = sum(v2)
+
+    # Sums of the squares
+    sum1sq = sum([pow(v, 2) for v in v1])
+    sum2sq = sum([pow(v, 2) for v in v2])
+
+    # Sum of products
+    productSum = sum([v1[i] * v2[i] for i in range(len(v1))])
+
+    # Calculate r (Pearson score)
+    # 1.0 when two items match perfectly
+    # 0.0 when there's no relationship at all
+    num = productSum - (sum1 * sum2 / len(v1))
+    den = sqrt((sum1sq - pow(sum1, 2) / len(v1)) *
+               (sum2sq - pow(sum2, 2) / len(v1)))
+    if den == 0:
+        return 0
+    return 1.0 - num / den
