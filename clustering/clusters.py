6051c9930d16f2c41c048b88a0fa855338630af1
Giorgix <jackosinapsis@gmail.com>
2015-01-30 20:34:34 +0100

    added tanamoto coefficent

--- clustering/clusters.py
+++ clustering/clusters.py
@@ -51,6 +51,19 @@ def pearson(v1, v2):
     return 1.0 - num / den
 
 
+def tanamoto(v1, v2):
+    c1, c2, shr = 0, 0, 0
+
+    for i in range(len(v1)):
+        if v1[i] != 0:
+            c1 += 1
+        if v2[i] != 0:
+            c2 += 1
+        if v1[i] != 0 and v2[i] != 0:
+            shr +=1
+    return 1.0 - (float(shr) / (c1 + c2 - shr))
+
+
 def hcluster(rows, distance=pearson):
     distances = {}
     currentclustid = -1
